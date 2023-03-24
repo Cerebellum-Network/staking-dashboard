1308a58248f9f8784cd90337693c5f435d323954
Ross Bulat <ross@parity.io>
2023-03-24 20:43:40 +0700

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/polkadot-staking-dashboard/',
   plugins: [
     eslint(),
     react(),
