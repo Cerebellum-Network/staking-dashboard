7df8f598fc4db2c8dc83bc8e32c9ae963d9bd3ea
Raid Ateir <ateirraid@gmail.com>
2024-01-24 06:28:15 +0100

    base for 1.0.9 prepared

---
+++ CHANGELOG.md
@@ -0,0 +1,131 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.5.0] - 2023-11-27
+
+## Added
+
+- Validators Community List Update. The following validators have been added: 
+  - Medium
+  - Jinogami
+  - EdgeServices
+  - Wombat 
+  - Brightlystake 
+  - Garm99
+  - 4T2CAPITAL
+  - TRK
+  - Testnetrun
+  - Tokem
+
+
+## [1.4.0] - 2023-10-11
+
+## Added
+
+- Validators Community List Update: Added AnyValid, SerGo and Xameyz
+
+## [1.3.0] - 2023-07-13
+
+## Added
+
+- Comprehensive application of a custom UI/UX theme, encompassing:
+    - Incorporation of specific colors for graph representation
+    - Accentuation of color for PayoutBar and PayoutLine elements
+    - Integration of the HumanSans typography
+    - Refinement of colors assigned to BalanceGraph and Bounded components
+    - Modification of the primary color dedicated to the light theme
+    - Refreshment of the Cere Icon and Logo aesthetics
+
+## Changed
+
+- The functionality to toggle between light and dark themes has been reinstated
+- Cere icon in the validator configuration has been updated
+
+## [1.2.1] - 2023-07-12
+
+## Changed
+
+- Updated README: Revised the instructions for Liveness under General Requirements and updated the Contribution Guide.
+  Additionally, included a feedback email for any questions or concerns regarding the content within the project.
+
+## [1.2.0] - 2023-07-11
+
+## Changed
+
+- Enabled the Community Page.
+- Updated the instructions for setting up the **Validator Entity** in the README.
+- Improved the display of validators' identities.
+
+## Added
+
+- Added validators for Cere.
+
+## [1.1.1] - 2023-06-01
+
+## Changed
+
+- Fix payout history date range. The dates were initially reversed and displayed the wrong date range.
+
+## [1.1.0] - 2023-05-31
+
+## Added
+
+- Added `CereStatsProvider` feature to fetch data from CereStats API, providing reward history and era points for
+  validators.
+- Implemented custom hooks, `useFetchEraPoints` and `usePayouts`, to manage data from CereStats API. `useFetchEraPoints`
+  retrieves era points for a specific validator, while `usePayouts` manages reward history.
+
+## Changed
+
+- The `useSubscan` hook has been replaced with the `useCereStats` hook for the Reward History and Era Point Charts data
+  source.
+
+## Removed
+
+- Removed dependency on `Subscan API` and simplified data management strategy by using `CereStats` API as primary data
+  source.
+
+## [1.0.1] - 2023-05-18
+
+## Added
+
+- Improved network initialization and error handling. Prevents application crashes caused by incorrect localStorage
+  network. Added default network for this scenario.
+
+### Changed
+
+- Improved workflow for deploying to development, staging, and production environments. Fixed issue
+  with `REACT_APP_INCLUDE_TESTNET` variable.
+- Set the `REACT_APP_INCLUDE_TESTNET` environment variable to `false` for the Stage environment.
+
+## [1.0.0] - 2023-05-17
+
+### Added
+
+- Google Tag Manager script
+- Cere Logo and Icon
+- Cere Mainnet and Testnet configurations
+- Cere Network favicons
+
+### Changed
+
+- Refactored testnet inclusion condition in build-and-deploy job
+- Updated branches for Stage and Prod
+- Updated information about Inflation
+- Fixed HTML page formatting, ChangeNominations, and CereUrl
+- Updated README.md, prod.yaml, Favicons, units, and URI Prefix
+- Adjusted constants and networks for Cere
+- Updated default Network and active networks fields
+
+### Removed
+
+- Binance fetch
+- maxNominatorsCount from TotalNominations
+- Feedback and Community pages
+- Settings Modal
+- UseInflation hook
+- Removed auctionCounter field
---
+++ src/contexts/CereStats/index.tsx
@@ -0,0 +1,181 @@
+import {
+  ApolloClient,
+  gql,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import React, { createContext, useEffect, useState } from 'react';
+import { Network } from '../../types';
+import { useApi } from '../Api';
+import { useConnect } from '../Connect';
+import { defaultCereStatsContext } from './defaults';
+import { CereStatsContextInterface } from './types';
+
+const useApolloClient = (endpoint: Network['cereStatsEndpoint']) => {
+  const [client, setClient] =
+    useState<ApolloClient<NormalizedCacheObject> | null>(null);
+
+  useEffect(() => {
+    const wsLink = new WebSocketLink({
+      uri: endpoint,
+      options: {
+        reconnect: true,
+      },
+    });
+
+    const _client = new ApolloClient({
+      link: wsLink,
+      cache: new InMemoryCache(),
+    });
+
+    setClient(_client);
+  }, [endpoint]);
+
+  return client;
+};
+
+const useFetchEraPoints = (
+  client: ApolloClient<NormalizedCacheObject> | null
+) => {
+  const fetchEraPoints = async (address: string, activeEraIndex: number) => {
+    if (!address || !client) {
+      return [];
+    }
+
+    const { data } = await client.query({
+      query: gql`
+        query EraPoints($stashAddress: String) {
+          era_points(where: { stash_address: { _eq: $stashAddress } }) {
+            era
+            points
+          }
+        }
+      `,
+      variables: { stashAddress: address },
+    });
+
+    if (data?.era_points !== null) {
+      const list = [];
+      // Set a constant for the number of eras we want to display
+      const ERAS_TO_SHOW = 100;
+
+      for (let i = activeEraIndex; i > activeEraIndex - ERAS_TO_SHOW; i--) {
+        list.push({
+          era: i,
+          reward_point:
+            data.era_points.find((item: any) => item.era === i)?.points ?? 0,
+        });
+      }
+      // removes last zero item and returns
+      return list.reverse().splice(0, list.length - 1);
+    }
+
+    return [];
+  };
+
+  return fetchEraPoints;
+};
+
+// Fetch Payouts Hook
+const usePayouts = (
+  client: ApolloClient<NormalizedCacheObject> | null,
+  activeAccount: string | null
+) => {
+  const [payouts, setPayouts] = useState([]);
+
+  const normalizePayouts = (
+    payoutData: { blockNumber: number; data: string; timestamp: number }[]
+  ) => {
+    return payoutData
+      .map(({ blockNumber, data, timestamp }) => {
+        let amount = 0;
+
+        // Using regex to extract the second parameter from the data string
+        const match = data.match(/,\s*(\d+)\s*\]/);
+
+        if (match && match[1]) {
+          amount = parseInt(match[1], 10);
+        }
+
+        return {
+          amount,
+          block_num: blockNumber,
+          block_timestamp: timestamp,
+        };
+      })
+      .sort((a, b) => b.block_timestamp - a.block_timestamp);
+  };
+
+  const fetchPayouts = async () => {
+    if (!activeAccount || !client) {
+      return;
+    }
+
+    const { data } = await client.query({
+      query: gql`
+        query RewardEvents($activeAccount: String) {
+          event(
+            where: {
+              section: { _eq: "staking" }
+              method: { _like: "Reward%" }
+              data: { _like: $activeAccount }
+            }
+          ) {
+            block_number
+            data
+            timestamp
+          }
+        }
+      `,
+      variables: {
+        activeAccount: `%${activeAccount}%`,
+      },
+    });
+
+    // @ts-ignore
+    setPayouts(normalizePayouts(data.event));
+  };
+
+  useEffect(() => {
+    fetchPayouts();
+  }, [client, activeAccount]);
+
+  return payouts;
+};
+
+const CereStatsContext = createContext<CereStatsContextInterface>(
+  defaultCereStatsContext
+);
+
+export const useCereStats = () => React.useContext(CereStatsContext);
+
+export const CereStatsProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { network } = useApi();
+  const { activeAccount } = useConnect();
+
+  const client = useApolloClient(network.cereStatsEndpoint);
+  const fetchEraPoints = useFetchEraPoints(client);
+  const payouts = usePayouts(client, activeAccount);
+
+  if (!client) {
+    return null;
+  }
+
+  return (
+    <CereStatsContext.Provider
+      value={{
+        fetchEraPoints,
+        payouts,
+        poolClaims: [],
+        unclaimedPayouts: [],
+      }}
+    >
+      {children}
+    </CereStatsContext.Provider>
+  );
+};
